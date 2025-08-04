878fc485830c6cbf451c5a7419a8f7aa0fcc2d50
bitcoingelato <bitcoingelato@protonmail.com>
2025-08-04 10:23:29 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); // switched from 'bcrypt' to 'bcryptjs'
 const jwt = require('jsonwebtoken');
 const { MongoClient } = require('mongodb');
 
